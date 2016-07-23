466aa8ec49880a058313ba0405fe55d58bda1101
Scott Carpenter <scottc@movingtofreedom.org>
2016-07-23 10:42:22 -0500

    handles lined bullets

--- liner.py
+++ liner.py
@@ -26,34 +26,35 @@ TEMP_FILE = '{home}/.liner_temp_file'.format(
 TEMP_FILE_LINED = '{temp}_lined'.format(temp=TEMP_FILE)
 LINED_SUFFIX = '_lined'
 UTF_8 = 'utf-8'
+BULLET_REGEX = r'^\s*[-*~] '
+
+line_length = DEFAULT_LINE_LENGTH
 
 
 def is_non_block(line):
     patterns = [
-        r'^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$', # date
-        r'^(~<|>~)',                             # excerpts
-        r'^\s*\|( |$)',                          # line quote
-        r'^(\s*:|\.\. )',                        # rst items
-        r'^[-#=~]{3,}',                          # separator/heading
+        r'^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$',  # date
+        r'^(~<|>~)',                              # excerpts
+        r'^\s*\|( |$)',                           # line quote
+        r'^(\s*:|\.\. )',                         # rst items
+        r'^[-#=~]{3,}',                           # separator/heading
     ]
 
     for pattern in patterns:
         if re.search(pattern, line):
             return True
 
-    if is_bullet(line):
-        return True
-
     return False
 
+
 def is_bullet(line):
-    if re.search(r'^\s*[-*~] ', line):
+    if re.search(BULLET_REGEX, line):
         return True
     else:
         return False
 
 
-def line_the_file(file_in, file_out, line_length=DEFAULT_LINE_LENGTH):
+def process_file(file_in, file_out):
 
     para = ''
     eol = ''
@@ -65,51 +66,36 @@ def line_the_file(file_in, file_out, line_length=DEFAULT_LINE_LENGTH):
         eol = '\n' if '\n' in line else ''
 
         line = line.rstrip()
-        if line == '' or is_non_block(line):
+        if line == '' or is_non_block(line) or is_bullet(line):
             if block_in_progress:
                 block_in_progress = False
                 # [:-1] to trim trailing space
-                write_paragraph(
-                    para[:-1],
-                    file_out,
-                    eol='\n',
-                    line_length=line_length
-                )
-            write_paragraph(
-                line,
-                file_out,
-                eol=eol,
-                line_length=line_length
-            )
-            para = ''
+                write_paragraph(para[:-1], file_out, eol='\n')
+                para = ''
+
+            if not is_bullet(line):
+                write_paragraph(line, file_out, eol=eol)
+                continue
+
+        block_in_progress = True
+        if para == '':
+            # preserve leading spaces; first line indicates
+            # blockquote indent for whole para (*if* a blockquote)
+            #
+            # also leading spaces important for bullets
+            para += line + ' '
         else:
-            block_in_progress = True
-            if para == '':
-                # preserve leading spaces; first line indicates
-                # blockquote indent for whole para (*if* a blockquote)
-                para += line + ' '
-            else:
-                para += line.strip() + ' '
+            para += line.strip() + ' '
 
     if para != '':
-        write_paragraph(
-            para[:-1],
-            file_out,
-            eol=eol,
-            line_length=line_length
-        )
+        write_paragraph(para[:-1], file_out, eol=eol)
 
     file_in.close()
     file_out.flush()
     file_out.close()
 
 
-def write_paragraph(
-    para,
-    file_out,
-    eol='\n',
-    line_length=DEFAULT_LINE_LENGTH
-):
+def write_paragraph(para, file_out, eol='\n'):
 
     if para == '' or is_non_block(para):
         file_out.write(para + eol)
@@ -126,6 +112,17 @@ def write_paragraph(
     else:
         para = para.lstrip()
         length = str(line_length - len(indent))
+
+    if is_bullet(para):
+        bullet_indent = re.sub(
+            r'(' + BULLET_REGEX + r').*$',
+            r'\1',
+            para
+        )
+        new_indent = indent + re.sub(r'\S', ' ', bullet_indent)
+        new_length = str(int(length) - 2)
+    else:
+        new_indent = None
 
     pattern = r'(.{0,' + length + r'}(?![^\s])|[^\s]+)\s*'
     r = re.compile(pattern)
@@ -149,6 +146,12 @@ def write_paragraph(
             # zero-width match; keep things moving along
             # (this may be impossible with our regex)
             m_end += 1  # pragma: no cover
+
+        if new_indent:
+            indent = new_indent
+            pattern = r'(.{0,' + new_length + r'}(?![^\s])|[^\s]+)\s*'
+            r = re.compile(pattern)
+            new_indent = None
 
         m = r.search(para, m_end)
 
@@ -195,6 +198,9 @@ def main(argv=None):
 
     args = ArgHandler.get_args(argv)
 
+    global line_length
+    line_length = args.line_length
+
     if args.file:
         file_out = '{filepath}{suffix}'.format(
             filepath=args.file,
@@ -202,20 +208,18 @@ def main(argv=None):
         )
         write_file(file_out, '')  # make sure is empty
 
-        line_the_file(
+        process_file(
             get_file_in(args.file),
-            get_file_out(file_out),
-            args.line_length
+            get_file_out(file_out)
         )
     elif args.clipboard:
         # clipboard uses files for consistent handling...
         write_file(TEMP_FILE, get_clipboard_data())
         write_file(TEMP_FILE_LINED, '')
 
-        line_the_file(
+        process_file(
             get_file_in(TEMP_FILE),
-            get_file_out(TEMP_FILE_LINED),
-            args.line_length
+            get_file_out(TEMP_FILE_LINED)
         )
 
         set_clipboard_data(read_file(TEMP_FILE_LINED))
@@ -223,10 +227,9 @@ def main(argv=None):
         os.remove(TEMP_FILE_LINED)
     else:
         sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
-        line_the_file(
+        process_file(
             codecs.getreader('utf-8')(sys.stdin),
-            sys.stdout,
-            args.line_length
+            sys.stdout
         )
 
 if __name__ == '__main__':
